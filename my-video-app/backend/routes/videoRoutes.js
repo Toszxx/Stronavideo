d0065711c0d2b623830408639ccb250acf7d1250
bonk-dev <github.p0j6t@passinbox.com>
2024-11-11 22:09:12 +0100

    Zastąp baze danych bazą in-memory

--- my-video-app/backend/routes/videoRoutes.js
+++ my-video-app/backend/routes/videoRoutes.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const Video = require("../models/Video");
+
+const twoja_zajebista_baza_danych = [];
 
 // Get all videos
 router.get("/", async (req, res) => {
-    const videos = await Video.find();
-    res.json(videos);
+    // const videos = await Video.find();
+    res.json(twoja_zajebista_baza_danych);
 });
 
 // Add a new video
 router.post("/", async (req, res) => {
     const { title, url, addedBy } = req.body;
-    const newVideo = new Video({ title, url, addedBy });
-    await newVideo.save();
+    const newVideo = { title, url, addedBy };
+    twoja_zajebista_baza_danych.push(newVideo);
     res.json(newVideo);
 });
 
